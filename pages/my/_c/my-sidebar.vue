d08fd229f2a4343fa2762c5872b6e53beeafcc80
dengshen <dengshendengshen@gmail.com>
2018-07-11 18:37:20 +0800

    feat: add order detail page layout

--- pages/my/_c/my-sidebar.vue
+++ pages/my/_c/my-sidebar.vue
@@ -1,10 +1,5 @@
 <style lang="scss">
   .my-sidebar {
-    .user-mini-profile {
-      padding: 0 18px 18px 18px;
-      border-bottom: solid 1px #eeeeee;
-    }
-
     .my-sidebar-menu {
       margin-top: 10px;
     }
@@ -60,30 +55,7 @@
 <template>
   <div class="my-sidebar" v-if="user.data">
     <CBlock class="my-sidebar-info" :x="0" :y="20">
-      <UserMiniProfile class="user-mini-profile" :username="user.data.username" :online="true"/>
-      <div class="pt-20 px-20">
-        <SidebarInfoItem title="30天成交量">
-          <Language text="[t][/t]单">
-            <span slot="t" class="c-brand-yellow">{{user.data.recent_deal_count || 0}} </span>
-          </Language>
-        </SidebarInfoItem>
-
-        <SidebarInfoItem title="30天完成率">
-          <span class="c-brand-yellow">{{userOrderCompleteRatio}} </span>
-        </SidebarInfoItem>
-
-        <SidebarInfoItem title="平均付款时间">
-          <Language text="[t][/t]分钟">
-            <span slot="t" class="c-brand-yellow">{{user.data.average_pay_time}} </span> <!--空格勿删-->
-          </Language>
-        </SidebarInfoItem>
-
-        <SidebarInfoItem title="平均放行时间">
-          <Language text="[t][/t]分钟">
-           <span slot="t" class="c-brand-yellow">{{user.data.average_release_time}} </span>
-          </Language>
-        </SidebarInfoItem>
-      </div>
+      <UserStatsProfile :user-data="user.data"/>
     </CBlock>
 
     <b-list-group class="my-sidebar-menu">
@@ -108,40 +80,17 @@
 </template>
 
 <script>
-  import Vue from 'vue'
   import {mapState} from 'vuex'
-  import UserMiniProfile from '~/components/user-mini-profile.vue'
-
-  const SidebarInfoItem = Vue.extend({
-    props: {
-      title: String,
-    },
-    render() {
-      return (
-        <div class="sidebar-info-item">
-          <span class="sidebar-info-item-title">{this.title}:</span>
-          {this.$slots['default']}
-        </div>
-      )
-    }
-  })
+  import UserStatsProfile from '~/components/user-stats-profile.vue'
 
   export default {
     components: {
-      UserMiniProfile,
-      SidebarInfoItem,
+      UserStatsProfile,
     },
     data() {
-      return {
-      }
+      return {}
     },
     computed: {
-      // 用户单子完成率
-      userOrderCompleteRatio: function () {
-        const userData = this.user.data
-        // todo: 这里根据后台返回数据，来显示  0% 和 -
-        return userData.recent_deal_count ? (userData.recent_deal_count / userData.recent_order_count * 100).toFixed(1) + '%' : '-'
-      },
       ...mapState(['user'])
     },
     beforeMount() {
---
+++ pages/orders/_id.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page-order-detail">
+    <div class="main-content"></div>
+    <div class="sidebar">
+      <CBlock class="my-sidebar-info" :x="0" :y="20">
+        <UserStatsProfile :user-data="merchant" v-if="merchant"/>
+      </CBlock>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+  .page-order-detail {
+    padding: 40px 0 118px 0px;
+    width: 1200px;
+    margin: 0 auto;
+    min-height: 900px;
+    display: flex;
+    .main-content {
+      flex: 1;
+      margin: -10px; //解决内容边界有阴影被隐藏的问题
+      padding: 10px;
+      overflow: hidden;
+    }
+    .sidebar {
+      width: 400px;
+      margin-left: 20px;
+    }
+  }
+</style>
+<script>
+  import UserStatsProfile from '~/components/user-stats-profile.vue'
+
+  export default {
+    data() {
+      return {
+        merchant: null,
+      }
+    },
+    components: {
+      UserStatsProfile,
+    }
+  }
+</script>
