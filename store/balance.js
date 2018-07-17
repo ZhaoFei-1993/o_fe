a68fb43e78882f950aa060c16150e28357ee898d
jeffjing <zgayjjf@qq.com>
2018-07-17 20:27:30 +0800

    feat:增加item相关接口；增加balance的映射

--- store/balance.js
+++ store/balance.js
@@ -1,16 +1,37 @@
 export default () => {
   const state = {
-    otcBalance: null,
+    otcBalance: null,       // 数组形式的balance
     coinexBalance: null,
+    otcMap: null,           // 键值对形式的balance
+    coinexMap: null,
+    currentCash: 'CNY',     // 当前的定价货币（法币）
+    allRate: null,             // 汇率
+    currentRate: null,      // 当前法币对应的汇率
   }
 
   const mutations = {
     SET_OTC_BALANCE(state, data) {
       state.otcBalance = data
+
+      state.otcMap = data.reduce((reduced, coin) => {
+        reduced[coin.coin_type] = coin
+        return reduced
+      }, {})
     },
     SET_COINEX_BALANCE(state, data) {
       state.coinexBalance = data
+
+      // 同时生成 key => value 的映射
+      state.coinexMap = data.reduce((reduced, coin) => {
+        reduced[coin.coin_type] = coin
+        return reduced
+      }, {})
     },
+
+    SET_EXCHANGE_RATE(state, data) {
+      state.allRate = data
+      state.currentRate = data[state.currentCash]
+    }
   }
 
   const actions = {
@@ -24,6 +45,11 @@ export default () => {
         commit('SET_COINEX_BALANCE', res.data)
       })
     },
+    fetchExchangeRate({commit}) {
+      return this.app.axios.balance.exchangeRate().then(res => {
+        commit('SET_EXCHANGE_RATE', res.data)
+      })
+    }
   }
 
   return {
