e56d4f58c0773f45a6a3bd7f17044826e19e218d
dengshen <dengshendengshen@gmail.com>
2018-08-03 16:46:08 +0800

    fix: temp use data in code

--- services/misc.js
+++ services/misc.js
@@ -49,11 +49,13 @@ export default (axios) => {
       // return Promise.resolve(require('./mock/misc').announcements)
       // https://static.coinex.com/viaconfig/otc-frontend/announcements/endpoint.json
       // 实际上最好用上面的cdn链接，但是因为cdn的header需要配置，不然会有跨域问题，所以只能暂时用s3的源链接了（s3已经配置了跨域头）
-      return axios.get('https://s3-ap-northeast-1.amazonaws.com/coinexstatic/viaconfig/otc-frontend/announcements/endpoint.json', {
-        headers: {
-          Authorization: ''     // 不可以带authorization,不然aws会不认
-        }
-      })
+      return Promise.resolve(
+        {
+          "announcements": [{
+            "title": "场外交易（OTC）隆重上线 交易、发布广告限时零费率",
+            "href": "https://support.coinex.com/hc/zh-cn/articles/360007646574"
+          }]
+        })
     }
   }
 }
