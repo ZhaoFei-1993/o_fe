6bee67a1474312472cc2eb53ba315e69b13c3add
dengshen <dengshendengshen@gmail.com>
2018-08-03 12:57:59 +0800

    clean: remove mock json server

--- modules/variables.js
+++ modules/variables.js
@@ -20,7 +20,7 @@ if (MODE === 'pre') {
   serverApiDomain = '//test.otc.coinex.com/res'
 }
 
-// 这个配置host到本地nginx之后就可以转发到想用的mock server了(localhost:4006)
+
 if (MODE === 'development') {
   webDomain = '//dev.otc.coinex.com'
   clientApiDomain = '//dev.otc.coinex.com/res'
