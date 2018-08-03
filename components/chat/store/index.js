046b0302aa324020a61185993484e61368095461
leolin1229 <leolin1229@gmail.com>
2018-08-03 11:21:27 +0800

    feat: 同步coinex的logger模块

--- components/chat/store/index.js
+++ components/chat/store/index.js
@@ -1,14 +1,14 @@
 import { Realtime } from 'leancloud-realtime'
 import AV from 'leancloud-storage'
 
-const MODE = process.env.MODE || 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
 export default () => {
-  let APP_ID = 'OibOYNHFsWoqChdhAlebT7rS-gzGzoHsz'
-  let APP_KEY = 'IEIfsx2I6LkRajvtP2jcoCIW'
-  if (MODE === 'production') {
-    APP_ID = 'yeIzHy3DnjcFwsBXWg61onrU-gzGzoHsz'
-    APP_KEY = '3ncyYb7rdSog1Yyvj0UVwydU'
+  let APP_ID = 'yeIzHy3DnjcFwsBXWg61onrU-gzGzoHsz' // 线上key
+  let APP_KEY = '3ncyYb7rdSog1Yyvj0UVwydU'
+  if (isDev) {
+    APP_ID = 'OibOYNHFsWoqChdhAlebT7rS-gzGzoHsz' // 测试key
+    APP_KEY = 'IEIfsx2I6LkRajvtP2jcoCIW'
   }
 
   const state = {
