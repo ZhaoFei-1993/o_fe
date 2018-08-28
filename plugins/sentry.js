8c2791d3a6144c6b54504a8b873acb9308d62aee
dengshen <dengshendengshen@gmail.com>
2018-08-28 14:45:57 +0800

    feat: add sample rate

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
 
+const sampleRate = 0.8
 if (process.env.MODE === 'production') {
   Raven
-    .config('https://920df9cbd56a4eaca855de0e4ab69536@sentry.coinex.com/2')
+    .config('https://920df9cbd56a4eaca855de0e4ab69536@sentry.coinex.com/2', {
+      shouldSendCallback: function () {
+        return (Math.random() <= sampleRate)
+      }
+    })
     .addPlugin(RavenVue, Vue)
     .install()
 }
